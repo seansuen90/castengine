ace197a54d2c9d334f163b6106555b393f6d78fd
seansuen90 <seansuen90@gmail.com>
2026-02-05 06:58:46 +0000

    imp ping pong logic for websocket server

--- src/ws/server.js
+++ src/ws/server.js
@@ -20,9 +20,18 @@ export function attachWebSocketServer(server) {
         maxPayload: 1024 * 1024,
     });
     wss.on('connection', (socket) => {
+        socket.on('pong', () => {socket.isAlive = true;});
         sendJson(socket, {type: 'welcome'});
         socket.on('error', console.error);
     });
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (ws.isAlive === false) return ws.terminate();
+        })
+        ws.isAlive = false;
+        ws.ping();
+    }, 30000);
+    wss.on('close', () => clearInterval(interval));
     function broadcastMatchCreated(match) {
         broadcast(wss, {type: 'match_created', data: match});
     }
